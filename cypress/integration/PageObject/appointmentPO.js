5a94a608be679c80be8f624e9ef06149cf73381a
yorhanpolanco <yorhanpolanco@gmail.com>
2022-03-29 01:04:40 -0400

    Primer cambio cypress

---
+++ cypress/integration/PageObject/appointmentPO.js
@@ -0,0 +1,140 @@
+
+class POappointment {
+
+   loadPage() {
+       cy.visit("http://localhost:3000/");
+
+       this.appNamelbl.contains('APPOINTMENT MANAGEMENT', {
+           matchCase: false
+       }).should('be.visible');
+   }
+
+   get appNamelbl() {
+       return cy.get('[data-testid="app-name"]');
+   }
+
+   get titlelbl() {
+       return cy.get('[data-testid="Title"]');
+   }
+
+   get petNametb() {
+       return cy.get('[data-testid="pet"]');
+   }
+   get ownerNametb() {
+       return cy.get('[data-testid="owner"]');
+   }
+   get datetb() {
+       return cy.get('[data-testid="date"]');
+   }
+   get timetb() {
+       return cy.get('[data-testid="time"]');
+   }
+   get symptomstb() {
+       return cy.get('[data-testid="symptoms"]');
+   }
+   get addAppoinmentbtn() {
+       return cy.get('[data-testid="btn-submit"]');
+   }
+
+   get dynamictitlelbl() {
+       return cy.get('[data-testid="dynamic-title"]');
+   }
+
+   get appointmentCreated() {
+       return cy.get('[data-testid="appointment"]');
+   }
+
+   get deletebtn() {
+       return cy.get('[data-testid="btn-delete"]');
+   }
+
+   get alert(){
+       return cy.get('[data-testid="alert"]');
+   }
+
+   
+
+   validateAppointment1(jsondata) {
+      
+
+       for (const [key, value] of Object.entries(jsondata)) {
+
+
+           this.appointmentCreated.each(($appointmentCreated) => {
+
+               var s = key + ": " + value;
+               var count = Object.keys(jsondata).length;
+               var acc = 0;
+
+               
+               cy.get($appointmentCreated).find('p').each(($p) => {
+                   console.log("acumalador" + acc);
+                   acc = ++acc;
+                   if (acc <= count) {
+                       if ($p.text().includes(s)) {
+                        
+                           console.log("Comparado " + $p.text() + " " + s);
+                        
+                           return true;
+
+                       }
+                   }
+               })
+           })
+       }
+   }
+
+
+   validateAppointment(jsondata) {  
+        this.appointmentCreated.each(($appointmentCreated,$i) => {
+
+            if(!this.dataset($appointmentCreated,jsondata)){
+                this.deletebtn.eq($i).should('be.enabled');
+                return false;
+            }
+
+          })
+    
+}
+
+    dataset(appointmentCreated,jsondata){
+        for (const [key, value] of Object.entries(jsondata)) {
+            
+            let count = Object.keys(jsondata).length;
+            
+            if(!this.item(appointmentCreated,count,key,value) ){
+                continue;
+            }else{
+                return false;
+            }
+
+        }
+    }
+
+    item(appointmentCreated,count,key,value){
+        let s = key + ": " + value;
+        let acc=0;
+        
+        cy.get(appointmentCreated).find('p').each(($p) => {
+            console.log("acumalador" + acc);
+
+            acc = ++acc;
+            if (acc <= count) {
+                if (s.includes($p.text())) {
+                 
+                    console.log("Comparado " + $p.text() + " " + s);
+                    acc = ++acc;                 
+                    return false;
+
+                }
+            }else{
+                
+                return true;}
+        })
+    }
+   
+    
+
+
+}
+export default new POappointment()
---
+++ cypress/integration/TestCase/appointment.spec.js
@@ -0,0 +1,80 @@
+///<reference types - "cypress">
+
+
+
+import POappointment from "../PageObject/appointmentPO"
+
+describe('Challenge',function(){
+
+    beforeEach(function(){
+        cy.fixture('challenge/testdata').then(function (data) {
+            this.data=data
+            cy.log('DATA: ',this.data.pet)
+        })
+         }
+         )   
+
+
+
+it('validate form fields',()=>{
+    
+    POappointment.loadPage()
+    POappointment.appNamelbl.should('be.visible');
+    POappointment.titlelbl.should('be.visible');
+    POappointment.petNametb.should('be.visible');
+    POappointment.ownerNametb.should('be.visible');
+    POappointment.datetb.should('be.visible');
+    POappointment.timetb.should('be.visible');
+    POappointment.symptomstb.should('be.visible');
+    POappointment.addAppoinmentbtn.should('be.visible');
+    POappointment.dynamictitlelbl.should('be.visible').contains('THERE ARE NO APPOINTMENTS',{matchCase:false});
+})
+
+it('validate that appointments can be deleted',function(){
+
+    POappointment.loadPage()
+
+    POappointment.petNametb.type(this.data.Pet);
+    POappointment.ownerNametb.type(this.data.Owner);
+    POappointment.datetb.type(this.data.Date);
+    POappointment.timetb.type(this.data.Time);
+    POappointment.symptomstb.type(this.data.Symptoms);
+    POappointment.addAppoinmentbtn.click();
+    POappointment.dynamictitlelbl.should('be.visible').contains('MANAGE YOUR APPOINTMENTS',{matchCase:false});
+    POappointment.deletebtn.click();
+    POappointment.deletebtn.should('not.exist');
+    
+
+})
+
+it('validate that appointments can be created',function(){
+
+
+
+    POappointment.petNametb.type(this.data.Pet);
+    POappointment.ownerNametb.type(this.data.Owner);
+    POappointment.datetb.type(this.data.Date);
+    POappointment.timetb.type(this.data.Time);
+    POappointment.symptomstb.type(this.data.Symptoms);
+    POappointment.addAppoinmentbtn.click();
+    POappointment.dynamictitlelbl.should('be.visible').contains('MANAGE YOUR APPOINTMENTS',{matchCase:false});
+    POappointment.validateAppointment(this.data);
+
+})
+
+it('validate that appointments cannot be created empty',function(){
+
+    POappointment.petNametb.clear();
+    POappointment.ownerNametb.clear();
+    POappointment.datetb.clear();
+    POappointment.timetb.clear();
+    POappointment.symptomstb.clear();
+    POappointment.addAppoinmentbtn.click();
+    POappointment.alert.should('be.visible').contains('ALL FIELDS ARE REQUIRED',{matchCase:false});
+    
+
+})
+
+
+
+})
